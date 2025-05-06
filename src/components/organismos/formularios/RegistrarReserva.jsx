66cfe349deb3ac78fce169d726205c3c3ba16a5b
kevin <2cv3695@gmail.com>
2025-05-06 10:06:54 -0500

    Agrego importaciones

---
+++ src/components/organismos/formularios/RegistrarReserva.jsx
@@ -0,0 +1,166 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+import { v } from "../../../styles/variables";
+import {
+    InputText,
+    Btnsave,
+    useReservaStore,
+    useEmpresaStore,
+} from "../../../index";
+import { useForm } from "react-hook-form";
+
+export function RegistrarReserva({ onClose, dataSelect, accion }) {
+    const { insertarReserva, editarReserva } = useReservaStore();
+    const { dataempresa } = useEmpresaStore();
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+
+    async function insertar(data) {
+        if (accion === "Editar") {
+            const p = {
+                id: dataSelect.id,
+                nombre: data.nombre,
+                cliente: data.cliente,
+                fecha: data.fecha,
+            };
+            await editarReserva(p);
+            onClose();
+        } else {
+            const p = {
+                _nombre: data.nombre,
+                _cliente: data.cliente,
+                _fecha: data.fecha,
+                _idempresa: dataempresa.id,
+            };
+            await insertarReserva(p);
+            onClose();
+        }
+    }
+
+    useEffect(() => {
+        if (accion === "Editar") {
+            // Opcional: cargar info adicional si se necesita
+        }
+    }, []);
+
+    return (
+        <Container>
+            <div className="sub-contenedor">
+                <div className="headers">
+                    <section>
+                        <h1>
+                            {accion === "Editar"
+                                ? "Editar reserva"
+                                : "Registrar nueva reserva"}
+                        </h1>
+                    </section>
+                    <section>
+                        <span onClick={onClose}>x</span>
+                    </section>
+                </div>
+
+                <form className="formulario" onSubmit={handleSubmit(insertar)}>
+                    <section>
+                        <article>
+                            <InputText icono={<v.iconoreserva />}>
+                                <input
+                                    className="form__field"
+                                    defaultValue={dataSelect?.nombre}
+                                    type="text"
+                                    placeholder=""
+                                    {...register("nombre", { required: true })}
+                                />
+                                <label className="form__label">Nombre</label>
+                                {errors.nombre && <p>Campo requerido</p>}
+                            </InputText>
+
+                            <InputText icono={<v.iconocliente />}>
+                                <input
+                                    className="form__field"
+                                    defaultValue={dataSelect?.cliente}
+                                    type="text"
+                                    placeholder=""
+                                    {...register("cliente", { required: true })}
+                                />
+                                <label className="form__label">Cliente</label>
+                                {errors.cliente && <p>Campo requerido</p>}
+                            </InputText>
+
+                            <InputText icono={<v.iconocalendario />}>
+                                <input
+                                    className="form__field"
+                                    defaultValue={dataSelect?.fecha}
+                                    type="date"
+                                    placeholder=""
+                                    {...register("fecha", { required: true })}
+                                />
+                                <label className="form__label">Fecha</label>
+                                {errors.fecha && <p>Campo requerido</p>}
+                            </InputText>
+                        </article>
+
+                        <div className="btnguardarContent">
+                            <Btnsave
+                                icono={<v.iconoguardar />}
+                                titulo="Guardar"
+                                bgcolor="#ef552b"
+                            />
+                        </div>
+                    </section>
+                </form>
+            </div>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+  transition: 0.5s;
+  top: 0;
+  left: 0;
+  position: fixed;
+  background-color: rgba(10, 9, 9, 0.5);
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+
+  .sub-contenedor {
+    width: 500px;
+    max-width: 85%;
+    border-radius: 20px;
+    background: ${({ theme }) => theme.bgtotal};
+    box-shadow: -10px 15px 30px rgba(10, 9, 9, 0.4);
+    padding: 13px 36px 20px 36px;
+    z-index: 100;
+
+    .headers {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 20px;
+
+      h1 {
+        font-size: 20px;
+        font-weight: 500;
+      }
+      span {
+        font-size: 20px;
+        cursor: pointer;
+      }
+    }
+
+    .formulario {
+      section {
+        gap: 20px;
+        display: flex;
+        flex-direction: column;
+      }
+    }
+  }
+`;
--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -1,27 +1,45 @@
-
 import { useQuery } from "@tanstack/react-query";
-import { useEmpresaStore } from "../store/EmpresaStore";
-import { SpinnerLoader } from "../components/moleculas/SpinnerLoader";
 import { ReservaTemplate } from "../components/templates/ReservaTemplate";
 import { useReservaStore } from "../store/ReservaStore";
+import { useEmpresaStore } from "../store/EmpresaStore";
+import { SpinnerLoader } from "../components/moleculas/SpinnerLoader";
+import { usePermisosStore, BloqueoPagina } from "../index";
 
-export function Reserva() {
-  const {mostrarReserva,datareserva,buscarReserva} = useReservaStore()
-  const {buscador} = useReservaStore();
-  const {dataempresa} = useEmpresaStore()
-  //mostrar data
- const {data,isLoading,error} = useQuery({queryKey:["mostrar reservas",dataempresa.id],queryFn:()=>mostrarReserva({id_empresa:dataempresa.id}),enabled:dataempresa.id!=null})
- //buscador
- const {data:buscar} = useQuery({queryKey:["buscar reservas",buscador],queryFn:()=>buscarReserva({descripcion: buscador,id_empresa:dataempresa.id}),enabled:dataempresa.id!=null})
- //respuestas
- if(isLoading){
-  return <SpinnerLoader/>
- }
- if(error){
-  return <span>Error...</span>
- }
-  return (<>
+export function Reservas() {
+  const { datapermisos } = usePermisosStore();
+  const statePermiso = datapermisos.some((objeto) =>
+    objeto.modulos.nombre.includes("Reservas")
+  );
+  if (!statePermiso) return <BloqueoPagina />;
 
-    <ReservaTemplate data={datareserva}/>
-  </>)
+  const { mostrarReservas, datareservas, buscarReservas, buscador } =
+    useReservaStore();
+  const { dataempresa } = useEmpresaStore();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["mostrar reservas", dataempresa.id],
+    queryFn: () => mostrarReservas({ idempresa: dataempresa.id }),
+    enabled: dataempresa.id != null,
+  });
+
+  const { data: buscar } = useQuery({
+    queryKey: ["buscar reservas", buscador],
+    queryFn: () =>
+      buscarReservas({ cliente: buscador, id_empresa: dataempresa.id }),
+    enabled: dataempresa.id != null,
+  });
+
+  if (isLoading) {
+    return <SpinnerLoader />;
+  }
+
+  if (error) {
+    return <span>Error...</span>;
+  }
+
+  return (
+    <>
+      <ReservaTemplate data={datareservas} />
+    </>
+  );
 }
